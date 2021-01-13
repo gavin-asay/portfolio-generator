426f4c7048abf436d9a84f09453ed48cb52353cb
Gavin Asay <gavin.asay@gmail.com>
2021-01-13 02:40:18 -0700

    inquirer installed

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [username, github] = profileDataArgs;
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
+// const pageHTML = generatePage(username, github);
 
-fs.writeFile("index.html", generatePage(username, github), err => {
-	if (err) throw err;
+// fs.writeFile("index.html", pageHTML, err => {
+// 	if (err) throw err;
 
-	console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// 	console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+
+inquirer
+	.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "What is your name?",
+		},
+	])
+	.then(answers => console.log(answers));
