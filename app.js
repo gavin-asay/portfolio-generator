84c1929825b2c59ffb6d69337dd0803f60818b5a
Gavin Asay <gavin.asay@gmail.com>
2021-01-14 03:51:47 -0700

    response handling

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
-// const pageHTML = generatePage(username, github);
-
-// fs.writeFile("index.html", pageHTML, err => {
-// 	if (err) throw err;
-
-// 	console.log("Portfolio complete! Check out index.html to see the output!");
-// });
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
 const promptUser = () => {
 	return inquirer.prompt([
@@ -140,4 +133,12 @@ const promptProject = portfolioData => {
 
 promptUser()
 	.then(promptProject)
-	.then(portfolioData => console.log(portfolioData));
+	.then(portfolioData => {
+		const pageHTML = generatePage(portfolioData);
+		fs.writeFile("index.html", pageHTML, err => {
+			if (err) throw err;
+			console.log(
+				"Portfolio complete! Check out index.html to see the output!"
+			);
+		});
+	});
