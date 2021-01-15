e851887b1f917b167ee06d6309793283585e3053
Gavin Asay <gavin.asay@gmail.com>
2021-01-15 03:08:15 -0700

    finished site

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const generatePage = require("./src/page-template.js");
 
 const promptUser = () => {
@@ -133,12 +133,11 @@ const promptProject = portfolioData => {
 
 promptUser()
 	.then(promptProject)
-	.then(portfolioData => {
-		const pageHTML = generatePage(portfolioData);
-		fs.writeFile("index.html", pageHTML, err => {
-			if (err) throw err;
-			console.log(
-				"Portfolio complete! Check out index.html to see the output!"
-			);
-		});
-	});
+	.then(portfolioData => generatePage(portfolioData))
+	.then(pageHTML => writeFile(pageHTML))
+	.then(writeFileResponse => {
+		console.log(writeFileResponse);
+		return copyFile();
+	})
+	.then(copyFileResponse => console.log(copyFileResponse))
+	.catch(err => console.log(err));
